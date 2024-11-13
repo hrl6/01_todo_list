9f523ec43d9052d31d77c8b0d5903c53fd23f0a1
hairul <hrulhadi@gmail.com>
2024-11-13 12:47:55 +0800

    - remove comment line for delete operation to work

--- delete.php
+++ delete.php
@@ -1,10 +1,8 @@
 <?php
-echo '<pre>';
-var_dump($_POST);
-echo '</pre>';
+//echo '<pre>';
+//var_dump($_POST);
+//echo '</pre>';
 
-
-/*
 if (file_exists('todo.json')) {
   $json = file_get_contents('todo.json');
   $jsonArray = json_decode($json, true);
@@ -18,5 +16,5 @@ unset($jsonArray[$todoName]);
 file_put_contents('todo.json', json_encode($jsonArray, JSON_PRETTY_PRINT));
 
 header('Location: index.php');
-*/
+
 ?>
